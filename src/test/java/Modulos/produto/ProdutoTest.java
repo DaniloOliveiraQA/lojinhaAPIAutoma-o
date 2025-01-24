0f40d6403cc470d9e87cc422c813b6ca0096319e
Dan Oliveira <dan.oliver03@hotmail.com>
2025-01-24 10:16:33 -0300

    Removi importacoes desnecessarios

--- src/test/java/Modulos/produto/ProdutoTest.java
+++ src/test/java/Modulos/produto/ProdutoTest.java
@@ -1,7 +1,5 @@
 package Modulos.produto;
 
-import groovyjarjarantlr.Token;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import pojo.Usuariopojo;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 @DisplayName("Testes de API Rest do modulo de Produto")
